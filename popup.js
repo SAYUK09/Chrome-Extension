0fa3bf73bb0baada7910da1cf3f36c81a84cfa4f
Sayuri Kamble <68416000+SAYUK09@users.noreply.github.com>
2021-10-23 16:11:03 +0530

    feat: buttons that set/clear alarms

--- popup.js
+++ popup.js
@@ -1,4 +1,21 @@
-chrome.alarms.create("PomodoroTaskAlarm", {
-  delayInMinutes: 1,
-  periodInMinutes: 1,
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("startBtn").addEventListener("click", setAlarm);
+
+  document.getElementById("stopBtn").addEventListener("click", clearAlarm);
 });
+
+function setAlarm() {
+  console.log("setting");
+  chrome.alarms.create("PomodoroTaskAlarm", {
+    delayInMinutes: 1,
+    periodInMinutes: 1,
+  });
+}
+
+function clearAlarm() {
+  console.log("clearing");
+
+  chrome.alarms.clear("PomodoroTaskAlarm", () => {
+    console.log("cleared");
+  });
+}
